0126dc5520a62b3f722a7f6eafb85f5a8eb21f5b
bagpipe360 <benjamin.matkin@me.com>
2015-01-10 03:18:11 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elp::Application.config.secret_token = '45d2babaaa14d667549ac42666722b2c14de985404ebb02f39505a6c1afe82beb30545f3dd656cbda8285eb180f96f343a5716523d2208c539151be13b17b39e'
